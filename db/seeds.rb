06e1c8f7fb7f503c5b5b3c44c579c90b6e6f1fcd
gauthier chabanne <gauthierchabanne@hotmail.fr>
2021-11-10 15:07:57 +0100

    Exercice 2 done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Starting seeding"
+
+restaurant1 = Restaurant.create(name: "McDo", address: "Maubeuge", category: "italian", phone_number: "0608192073")
+restaurant2 = Restaurant.create(name: "Burger King", address: "Paris", category: "french", phone_number: "0608192075")
+restaurant3 = Restaurant.create(name: "Starbucks", address: "Toulouse", category: "chinese", phone_number: "0618198075")
+restaurant4 = Restaurant.create(name: "Quick", address: "Paris", category: "belgium", phone_number: "0928836969")
+restaurant5 = Restaurant.create(name: "Subway", address: "Strasbourg", category: "japanese", phone_number: "0827371923")
+
+puts "Seeding Ended"
